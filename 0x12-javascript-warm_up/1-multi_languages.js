015eb25b24e58dd9ca9d04fcbfd2c2df8d2b42cf
Isaac-Oyediran <isaacoyediran57@gmail.com>
2022-10-05 18:26:42 +0100

    Create 1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let i;
+for (i = 1; i < 4; i++) {
+  if (i === 1) { console.log('C is fun'); }
+  if (i === 2) { console.log('Python is cool'); }
+  if (i === 3) { console.log('JavaScript is amazing'); }
+}
